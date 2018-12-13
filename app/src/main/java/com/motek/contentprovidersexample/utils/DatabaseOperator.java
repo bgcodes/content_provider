c73ea652bbda87ec79d438922d58252462295532
Grzegorz Bogdan <grzegorz.infs@gmail.com>
2018-12-13 03:30:43 +0100

    f2
    f2

--- app/src/main/java/com/motek/contentprovidersexample/utils/DatabaseOperator.java
+++ app/src/main/java/com/motek/contentprovidersexample/utils/DatabaseOperator.java
@@ -1,17 +1,22 @@
 package com.motek.contentprovidersexample.utils;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.text.TextUtils;
+
+import java.util.HashMap;
 
 public class DatabaseOperator {
     private String table;
     private String database;
     private SQLiteDatabase db;
 
-    public DatabaseOperator(String table, String database) {
+    public DatabaseOperator(String table, String database, SQLiteDatabase db) {
         this.table = table;
         this.database = database;
+        this.db = db;
     }
 
     public void insert(ContentValues values) {
@@ -23,17 +28,17 @@ public class DatabaseOperator {
                         (!TextUtils.isEmpty(selection) ? " AND (" + selection + ')' : ""), selectionArgs);
     }
 
+    public void update(String key, String selection, String[] selectionArgs, ContentValues values) {
+        db.update(table, values,
+                "key" + " = " + key +
+                        (!TextUtils.isEmpty(selection) ? " AND (" +selection + ')' : ""), selectionArgs);
+    }
+
     public Cursor buildQuery(String[] projection, String selection, String[] selectionArgs, String sortOrder) {
         SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
         qb.setTables(table);
         qb.setProjectionMap(new HashMap<String, String>());
 
         return qb.query(db,	projection,	selection, selectionArgs,null, null, sortOrder);
-    }
-    
-    public void update(String key, String selection, String[] selectionArgs, ContentValues values) {
-        db.update(table, values,
-                "key" + " = " + key +
-                        (!TextUtils.isEmpty(selection) ? " AND (" +selection + ')' : ""), selectionArgs);
     }
 }
